91fa876c51ba84bd3231712d61642865045d40b4
ykhaledw <youssefwahdan12@gmail.com>
2023-12-08 22:30:02 +0200

    finishing book details view

--- lib/features/book_details/presentation/views/widgets/books_details_view_body.dart
+++ lib/features/book_details/presentation/views/widgets/books_details_view_body.dart
@@ -46,6 +46,20 @@ class BookDetailsViewBody extends StatelessWidget {
               height: 37,
             ),
             const BookAction(),
+            const SizedBox(
+              height: 49,
+            ),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'You can also like',
+                style: Styles.textStyle14.copyWith(fontWeight: FontWeight.w600),
+              ),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            const SimilarBooksListView(),
           ],
         ),
       ),
@@ -53,4 +67,23 @@ class BookDetailsViewBody extends StatelessWidget {
   }
 }
 
+class SimilarBooksListView extends StatelessWidget {
+  const SimilarBooksListView({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * 0.15,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: 10,
+        itemBuilder: (context, index) {
+          return const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 5),
+            child: CustomBookImage(),
+          );
+        },
+      ),
+    );
+  }
+}
