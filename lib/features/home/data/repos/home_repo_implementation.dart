2390865705f1ce7a1b17d04e9c06e368d0782c81
ykhaledw <youssefwahdan12@gmail.com>
2023-12-12 07:22:52 +0200

    create similar books cubit

--- lib/features/home/data/repos/home_repo_implementation.dart
+++ lib/features/home/data/repos/home_repo_implementation.dart
@@ -47,4 +47,23 @@ class HomeRepoImplementation implements HomeRepo {
       return left(ServerFailure(e.toString()));
     }
   }
+  
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchSimilarBooks({required String category}) async{
+    try {
+      var data = await apiService.get(
+          endPoint: 'volumes?Filtering=free-ebooks&Sorting=relevance&q=subject:novels');
+
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.fromDioError(e));
+      }
+      return left(ServerFailure(e.toString()));
+    }
+  }
 }
---
+++ lib/features/home/presentation/views_model/similar_books_cubit/similar_books_cubit.dart
@@ -0,0 +1,19 @@
+import 'package:bookly/features/home/data/repos/home_repo.dart';
+import 'package:bookly/features/home/presentation/views_model/similar_books_cubit/similar_books_states.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class SimilarBooksCubit extends Cubit<SimilarBooksStates> {
+  SimilarBooksCubit(this.homeRepo) : super(SimilarBooksInitial());
+
+  final HomeRepo homeRepo;
+
+  Future<void> fetchSimilartBooks({required String category}) async {
+    emit(SimilarBooksLoading());
+    var result = await homeRepo.fetchSimilarBooks(category: category);
+    result.fold((failure) {
+      emit(SimilarBooksFailure(failure.errorMessage));
+    }, (books) {
+      emit(SimilarBooksSuccess(books));
+    });
+  }
+}
